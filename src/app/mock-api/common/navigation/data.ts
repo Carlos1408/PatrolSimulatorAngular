a24e9d6f98c0b105812cc87c1cb301f696757710
cclaurev <carlos.claure.vargas@gmail.com>
2023-10-18 00:17:22 -0400

    reports

--- src/app/mock-api/common/navigation/data.ts
+++ src/app/mock-api/common/navigation/data.ts
@@ -1,39 +1,81 @@
 /* tslint:disable:max-line-length */
 import { FuseNavigationItem } from '@fuse/components/navigation';
+import { environment } from 'environments/environment';
 
 export const defaultNavigation: FuseNavigationItem[] = [
     {
-        id      : 'dashboard',
-        title   : 'Dashboards',
+        id: 'dashboard',
+        title: 'Dashboards',
         subtitle: 'Panel de reportes del simulador',
-        type    : 'group',
-        icon    : 'heroicons_outline:home',
+        type: 'group',
+        icon: 'heroicons_outline:home',
         children: [
             {
-                id   : 'dashboard.maps',
+                id: 'dashboard.maps',
                 title: 'Panel de Control',
-                type : 'basic',
-                icon : 'heroicons_outline:chart-square-bar',
-                link : 'dashboard/maps'
-            }
-        ]
+                type: 'basic',
+                icon: 'heroicons_outline:chart-square-bar',
+                link: 'dashboard/maps',
+            },
+        ],
     },
     {
-        id   : 'admin',
+        id: 'admin',
         title: 'Administracion',
-        subtitle : 'Area de administracion',
-        type : 'group',
-        icon : 'heroicons_outline:cog',
+        subtitle: 'Area de administracion',
+        type: 'group',
+        icon: 'heroicons_outline:cog',
         children: [
-        {
-            id: 'users',
-            title : 'Usuarios',
-            type: 'basic',
-            icon: 'heroicons_outline:user',
-            link: 'admin/users'
-        }
-    ]
-    }
+            {
+                id: 'users',
+                title: 'Usuarios',
+                type: 'basic',
+                icon: 'heroicons_outline:user',
+                link: 'admin/users',
+            },
+        ],
+    },
+    {
+        id: 'reports',
+        title: 'Reportes',
+        subtitle: 'Generación de reportes',
+        type: 'group',
+        icon: 'heroicons_outline:cog',
+        children: [
+            {
+                id: 'reportPatrols',
+                title: 'Patrullajes',
+                type: 'basic',
+                icon: 'heroicons_outline:book-open',
+                link: `${environment.APIurl}/reports/patrols`,
+                externalLink: true,
+            },
+            {
+                id: 'reportRecognitions',
+                title: 'Reconocimientos',
+                type: 'basic',
+                icon: 'heroicons_outline:book-open',
+                link: `${environment.APIurl}/reports/recognitions`,
+                externalLink: true,
+            },
+            {
+                id: 'reportAmbushes',
+                title: 'Emboscadas',
+                type: 'basic',
+                icon: 'heroicons_outline:book-open',
+                link: `${environment.APIurl}/reports/ambushes`,
+                externalLink: true,
+            },
+            {
+                id: 'reportCombats',
+                title: 'Combates',
+                type: 'basic',
+                icon: 'heroicons_outline:book-open',
+                link: `${environment.APIurl}/reports/combats`,
+                externalLink: true,
+            },
+        ],
+    },
 ];
 export const compactNavigation: FuseNavigationItem[] = [
     {
@@ -41,16 +83,16 @@ export const compactNavigation: FuseNavigationItem[] = [
         title: 'Dashboard',
         type: 'basic',
         link: 'dashboard',
-        icon: 'heroicons_outline:chart-square-bar'
+        icon: 'heroicons_outline:chart-square-bar',
     },
     {
-        id   : 'admin',
+        id: 'admin',
         title: 'Administracion',
-        subtitle : 'Area de administracion de catalogos',
-        type : 'group',
-        icon : 'heroicons_outline:cog',
-        children: []
-    }
+        subtitle: 'Area de administracion de catalogos',
+        type: 'group',
+        icon: 'heroicons_outline:cog',
+        children: [],
+    },
 ];
 export const futuristicNavigation: FuseNavigationItem[] = [
     {
@@ -58,16 +100,16 @@ export const futuristicNavigation: FuseNavigationItem[] = [
         title: 'Dashboard',
         type: 'basic',
         link: 'dashboard',
-        icon: 'heroicons_outline:chart-square-bar'
+        icon: 'heroicons_outline:chart-square-bar',
     },
     {
-        id   : 'admin',
+        id: 'admin',
         title: 'Administracion',
-        subtitle : 'Area de administracion de catalogos',
-        type : 'group',
-        icon : 'heroicons_outline:cog',
-        children: []
-    }
+        subtitle: 'Area de administracion de catalogos',
+        type: 'group',
+        icon: 'heroicons_outline:cog',
+        children: [],
+    },
 ];
 export const horizontalNavigation: FuseNavigationItem[] = [
     {
@@ -75,14 +117,14 @@ export const horizontalNavigation: FuseNavigationItem[] = [
         title: 'Dashboard',
         type: 'basic',
         link: 'dashboard',
-        icon: 'heroicons_outline:chart-square-bar'
+        icon: 'heroicons_outline:chart-square-bar',
     },
     {
-        id   : 'admin',
+        id: 'admin',
         title: 'Administracion',
-        subtitle : 'Area de administracion de catalogos',
-        type : 'group',
-        icon : 'heroicons_outline:cog',
-        children: []
-    }
+        subtitle: 'Area de administracion de catalogos',
+        type: 'group',
+        icon: 'heroicons_outline:cog',
+        children: [],
+    },
 ];
